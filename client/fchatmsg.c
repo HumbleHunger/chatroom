7a98f9187a34384f310f931c3bdd7e6dc79f6731
HumbleHunger <2495970924@qq.com>
2020-08-25 22:48:52 +0800

    update

--- client/fchatmsg.c
+++ client/fchatmsg.c
@@ -19,7 +19,6 @@ int fchatmsg()
     char send_buf[1024];
     memset(send_buf,0,sizeof(send_buf));
     sprintf(send_buf,"%s\n%s\n",user_id,fid);
-    //printf("fchatmsg send_buf is %s",send_buf);//
     if(send_pack(connfd,FCHATMSG,strlen(send_buf),send_buf)<0){
         my_err("write",__LINE__);
     }
